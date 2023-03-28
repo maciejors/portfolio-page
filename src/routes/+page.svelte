d27e92b083468a371913a0cf10ca82c72bf633ea
maciejors <maciej.orslowski@gmail.com>
2023-03-28 22:39:25 +0200

    init project

---
+++ src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<div class="flex flex-col h-screen space-between items-center">
+	<div class="my-auto">
+		<h1 class="text-6xl font-bold text-center mb-2">WORK IN PROGRESS</h1>
+		<h2 class="text-3xl text-center">Please stand by</h2>
+	</div>
+</div>
