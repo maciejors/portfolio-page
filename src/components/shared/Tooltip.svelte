1d070d4c4ac990b4b1fe6d10c3ef329d512dc2db
maciejors <maciej.orslowski@gmail.com>
2023-08-29 10:51:54 +0200

    add delay to tooltips

--- src/components/shared/Tooltip.svelte
+++ src/components/shared/Tooltip.svelte
@@ -13,11 +13,11 @@
 	}
 
 	.tooltip-text {
-		@apply invisible absolute bottom-full left-1/2 -translate-x-1/2 z-20 mb-0.5;
+		@apply opacity-0 absolute bottom-full left-1/2 -translate-x-1/2 z-20 mb-1;
 		@apply bg-gray-700 text-white text-sm text-center rounded px-1.5 py-0.5 whitespace-nowrap;
 	}
 
 	.tooltip:hover .tooltip-text {
-		@apply visible;
+		@apply opacity-100 transition-opacity delay-700;
 	}
 </style>
