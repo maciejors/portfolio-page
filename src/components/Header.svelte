51aa17851795ef4c9169e117efe19c3266a1a387
maciejors <maciej.orslowski@gmail.com>
2023-08-31 17:46:14 +0200

    refactor header styles

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -3,13 +3,11 @@
 	import Tooltip from './shared/Tooltip.svelte';
 </script>
 
-<header class="w-full h-96 bg-cover bg-home bg-no-repeat">
-	<div
-		class="flex flex-col h-full justify-center items-center bg-white bg-opacity-40 px-4 backdrop-blur-sm"
-	>
-		<div class="card bg-blue-700 text-white p-4 flex flex-col items-center gap-4">
-			<h1 class="text-center">Maciej Orsłowski</h1>
-			<div class="flex flex-row items-center gap-6 h-8">
+<header>
+	<div class="profile-card-container">
+		<section class="card profile-card">
+			<h1>Maciej Orsłowski</h1>
+			<div class="socials-list">
 				<Tooltip tooltipText="Github" delayed>
 					<a href="https://github.com/maciejors">
 						<GithubIcon size="28" />
@@ -21,11 +19,31 @@
 					</a>
 				</Tooltip>
 			</div>
-		</div>
+		</section>
 	</div>
 </header>
 
 <style lang="postcss">
+	header {
+		@apply w-full h-96 bg-cover bg-home bg-no-repeat;
+	}
+
+	h1 {
+		@apply text-center;
+	}
+
+	.profile-card-container {
+		@apply flex flex-col h-full justify-center items-center bg-white bg-opacity-40 px-4 backdrop-blur-sm;
+	}
+
+	.profile-card {
+		@apply bg-blue-700 text-white p-4 flex flex-col items-center gap-4;
+	}
+
+	.socials-list {
+		@apply flex flex-row items-center gap-6 h-8;
+	}
+
 	a:hover {
 		@apply text-gray-200;
 	}
